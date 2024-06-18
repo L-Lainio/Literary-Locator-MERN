a92393c04df3e5234a6c2591a3d9d57322197e1d
L-Lainio <arollainio@gmail.com>
2024-06-18 00:17:29 -0400

    added to eslintrc.cjs

--- Develop/client/.eslintrc.cjs
+++ Develop/client/.eslintrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true // Add this line
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
